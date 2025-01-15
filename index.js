157e95bf25842690be2e7ff9cdd18e54619ccc6d
kelvinuwomano <jameskelvincruz69@gmail.com>
2025-01-15 11:29:52 +0100

    Added validation to my index.js

--- index.js
+++ index.js
@@ -18,3 +18,27 @@ const navMenu = document.getElementById("navMenu")
 menuBar.addEventListener("click", () =>{
     navMenu.classList.toggle("show")
 })
+function signUp() {
+    
+const userName = document.getElementById("signupUname").value;
+const email = document.getElementById("signupEmail").value;
+const password = document.getElementById("signupPass").value;
+
+const userData = { userName, email, password};
+localStorage.setItem("user", JSON.stringify(userData))
+
+}
+
+function login() {
+    const userName = document.getElementById("loginUname").value;
+    const email = document.getElementById("loginEmail").value;
+    const password = document.getElementById("loginPassword").value;
+
+    const storedUser = JSON.parse(localStorage.getItem("user"));
+    if (storedUser && storedUser.userName === userName && storedUser.email === email && storedUser.password === password) {
+        alert("LOGIN SUCCESSFUL")
+    } else {
+        document.getElementById("error").textContent = "Fill in your details correctly"
+    }
+}
+
